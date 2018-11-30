e1a22c43700cf8170751994dcbec07d3addeda83
Martin Hylleberg <martin.hylleberg@systematic.com>
2018-11-30 09:40:24 +0100

    temporary fix for repeatpipe module problem in prod setup

--- src/app/mh-utils/mh-utils.module.ts
+++ src/app/mh-utils/mh-utils.module.ts
@@ -1,13 +1,13 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RepeatPipe} from './repeat.pipe';
-
-@NgModule({
-  declarations: [RepeatPipe],
-  imports: [
-    CommonModule
-  ],
-  exports: [RepeatPipe]
-})
-export class MhUtilsModule {
-}
+// import {NgModule} from '@angular/core';
+// import {CommonModule} from '@angular/common';
+// import {RepeatPipe} from './repeat.pipe';
+//
+// @NgModule({
+//   declarations: [RepeatPipe],
+//   imports: [
+//     CommonModule
+//   ],
+//   exports: [RepeatPipe]
+// })
+// export class MhUtilsModule {
+// }
