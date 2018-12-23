9f7a023278d1d03cfa9cf7c5976b1dc48f6c0c05
Martin Hylleberg <mhylle@yahoo.com>
2018-12-23 13:04:22 +0100

    added description of element in the capabilityview

--- src/app/services/structure-definition.service.ts
+++ src/app/services/structure-definition.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
 import {ConfigurationService} from './infrastructure/configuration.service';
 import Bundle = fhir.Bundle;
 
@@ -21,14 +20,15 @@ export class StructureDefinitionService {
   private static handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
-      // console.error('An error occurred:', error.error.message);
+      console.error('An error occurred:', error.error.message);
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
-      // console.error(
-      //   `Backend returned code ${error.status}, ` +
-      //   `body was: ${error.error}`);
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
     }
+    console.log('error');
     // return an observable with a user-facing error message
     return throwError(
       'Something bad happened; please try again later.');
@@ -41,9 +41,9 @@ export class StructureDefinitionService {
     httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
     return this.http.get<Bundle>(
       this.configurationService.selectedServer + '/fhir/StructureDefinition/' + resource,
-      httpOptions)
-      .pipe(
-        catchError(StructureDefinitionService.handleError)
-      );
+      httpOptions);
+    // .pipe(
+    //   catchError(StructureDefinitionService.handleError)
+    // );
   }
 }
