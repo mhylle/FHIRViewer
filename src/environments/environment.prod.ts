67fab93a985fd32cca5ee9a28159418361b6114a
Martin Hylleberg <mhylle@yahoo.com>
2019-01-07 13:53:40 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  resourceServers: ['http://clud-13783-web:3023/columna', 'http://clud-13783-t70:18080/columna']
+  resourceServers: ['http://clud-13783-web:3023/columna']
 };
